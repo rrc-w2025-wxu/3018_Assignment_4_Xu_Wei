971bf5064947411b80b0b70293efd2cd37a30dc2
XU WEI <wxu48@academic.rrc.ca>
2026-03-14 12:49:42 -0500

    feat: add logger in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import { accessLogger, errorLogger, consoleLogger } from "./api/v1/middleware/logger";
 import morgan from "morgan";
+import errorHandler from "./api/v1/middleware/errorHandler";
 import Routes from "./api/v1/routes/Routes";
 
 /**
  * Initialize an Express application
  */
 const app = express();
+
+app.use(consoleLogger);
+
+app.use(accessLogger);
+app.use(errorLogger);
 
 // morgan middleware
 app.use(morgan("dev"));
@@ -34,6 +41,8 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(errorHandler);
 
 /**
  * Export the Express app instance for testing or further configuration
